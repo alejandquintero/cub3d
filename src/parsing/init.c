7dda79dea0cda056e70c04d7c3dd8da3a27ce968
lgandarillas <lgandaf@gmail.com>
2025-02-23 11:07:58 +0100

    Refactor init.c

--- src/parsing/init.c
+++ src/parsing/init.c
@@ -42,17 +42,14 @@ void	init(char *file)
 {
 	int	fd;
 
-	if (check_extension(file, ".cub") == true)
-	{
-		fd = open(file, O_RDONLY);
-		if (fd == -1)
-		{
-			perror("Error\nCould not open the file");
-			exit(1);
-		}
-		read_file(fd);
-		close(fd);
-	}
-	else
+	if (!check_extension(file, ".cub"))
 		print_error("Error\nInvalid file extension. It must be '.cub'\n");
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+	{
+		perror("Error\nCould not open the file");
+		exit(1);
+	}
+	read_file(fd);
+	close(fd);
 }
